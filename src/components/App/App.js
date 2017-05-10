d9da391ad3693ad3e95ec67ff6f810b621c36607
Arjun Raj <rajarju@gmail.com>
2017-05-10 15:51:50 +0530

    Added Routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,77 +1,27 @@
 import React from 'react';
-import TodoItem from '../TodoItem/TodoItem';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import TodoList from '../TodoList/TodoList';
+import {TodoItemPage} from '../TodoItem/TodoItem';
 import './App.css';
 
 class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = { items: [
-      { id:0, value : 'Todo 1', done: true},
-      { id:1, value : 'Todo 2', done: false},
-      { id:2, value : 'Todo 3', done: false},
-    ] }
-  }
-
-  componentDidMount() {
-    let items = this.state.items;
-    this.setState({
-      items : this.sortItems(items)
-    });
-  }
-
-  sortItems(items) {
-    items.sort((a, b) => (b.done === a.done) ? 0  : b.done ? -1 : 1);
-    return items;
-  }
-
-  toggleItem(id) {
-    let items = this.state.items;
-    const index = items.findIndex( item => item.id === id)
-    items[index].done = !items[index].done;
-    this.setState({
-      items : this.sortItems(items)
-    });
-  }
-
-  addTodo(e) {
-    if(e.keyCode === 13 && this.refs.newTodo.value !== "") {
-      let items = this.state.items;
-      const newItem = {
-        id : items.length + 1, // Or Random UUID,
-        value : this.refs.newTodo.value,
-        done : false
-      };
-      items.push(newItem);
-      this.setState({
-        items : this.sortItems(items)
-      });
-      this.refs.newTodo.value = "";
-    }
-
-  }
-
   render() {
-    let items = this.state.items;
-
     return(
-      <div className="App">
-        <h1 className="App__Title">Todo App</h1>
-
-        <div className="App__Todo--container container-shadow">
-          {items.map(item =>
-            <TodoItem key={item.id} item={item} toggle={this.toggleItem.bind(this)}></TodoItem>)}
-        </div>
-
-        <div className="App__Todo__Form ">
-          <textarea ref="newTodo" onKeyUp={this.addTodo.bind(this)}
-          placeholder="Add todo" className="App__Todo__Form--input container-shadow"></textarea>
-          <p className="App__Todo__Form--description">Hit Enter to submit</p>
-        </div>
-
-      </div>
-      );
-    }
+      <Router>
+          <div className="App">
+            <h1 className="App__Title">Todo App</h1>
+            <Route exact={true} path="/" component={TodoList} />
+            <Route path="/todo/:itemId" render={ ( {match} ) => (
+              <TodoItemPage match={match} />
+            ) }>
+              
+            </Route>          
+          </div>
+      </Router>
+    );
   }
+}
 
   export default App
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './TodoItem.css';
+import {Link} from 'react-router-dom';
+
 
 class TodoItem extends React.Component {
   constructor(props) {
@@ -18,15 +20,76 @@ class TodoItem extends React.Component {
   render() {
     var item = this.props.item;
     return (
-      <div  className={"TodoItem " + (item.done ? "TodoItem--done" : "")}>
-        <div className="TodoItem--text">{item.value}</div>
+      <div className={"TodoItem " + (item.done ? "TodoItem--done" : "")}>
+        <Link className="TodoItem--text" to={ `/todo/${item.id}` }>{item.value}</Link>
         <div className="TodoItem__Actions">
-        <input type="checkbox" checked={item.done} onChange={this.toggleItem.bind(this)} className="TodoItem__Actions__Action"/>
-        <button onClick={this.deleteItem.bind(this)} className="TodoItem__Actions__Action">Delete</button>
+          <input type="checkbox" checked={item.done}
+          onChange={this.toggleItem.bind(this)}
+          className="TodoItem__Actions__Action"/>
+
+          <button onClick={this.deleteItem.bind(this)}
+          className="TodoItem__Actions__Action">Delete</button>
         </div>
       </div>
     );
   }
 }
 
+
+class TodoItemPage extends React.Component {
+  constructor(params) {
+    super(params);
+    let match = params.match;
+    this.state = {
+      itemId : match.params.itemId
+    };
+  }
+
+  componentWillMount() {
+    let item = {
+      id : this.state.itemId,
+      value : 'Dynamic Item',
+      description: 'Some Large Description of the todo Item',
+      done: false
+    };
+    this.setState({ item })
+  }
+
+  toggleItem() {
+    let item = this.item;
+    item.done = !item.done;
+    this.setState({
+      item
+    });
+  }
+  
+
+  deleteItem() {
+
+  }
+
+  render() {
+    var item = this.state.item;
+    return (
+      <div>
+        <Link to={ `/` }><span>Back</span></Link>
+        <div className={"TodoItem " + (item.done ? "TodoItem--done" : "")}>
+          <Link className="TodoItem--text" to={ `/todo/${item.id}` }>{item.value}</Link>
+          <div className="TodoItem__Actions">
+            <input type="checkbox" checked={item.done}
+            onChange={this.toggleItem.bind(this)}
+            className="TodoItem__Actions__Action"/>
+            <button onClick={this.deleteItem.bind(this)}
+            className="TodoItem__Actions__Action">Delete</button>
+          </div>          
+        </div>
+        <div>
+            {item.description}
+        </div>
+      </div>
+    );
+  }
+}
+
+exports.TodoItemPage = TodoItemPage;
 export default TodoItem;
