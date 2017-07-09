cb88fa1636c4d40c61ed612a8ae7dac4e7912988
Arjun Raj <rajarju@gmail.com>
2017-07-09 12:59:10 +0530

    Remove spec

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore.js';
 import App from './components/App/App';
+
 import './index.css';
 
 const store = configureStore();
